e99bb7108e9aa52c043febb91d92ffb8275ea068
Joel Eidsath <jeidsath@gmail.com>
2014-07-26 10:44:39 -0700

    Comments!

---
+++ commenter.py
@@ -0,0 +1,13 @@
+import re
+
+class Commenter(object):
+    def __init__(self, text):
+        self.text = text
+
+    def comments_removed(self):
+        out = []
+
+        for line in self.text:
+            out.append(re.sub('#.*', '', line))
+
+        return out
--- lm.py
+++ lm.py
@@ -4,6 +4,21 @@ import argparse
 
 from functioneer import Functioneer
 from output import Outputer
+from commenter import Commenter
+
+
+def main():
+    args = parse()
+
+    # strip comments
+    text = Commenter(load(args.file)).comments_removed()
+
+    # replace functions
+    functioneer = Functioneer(text)
+    functioneer.get_functions()
+
+    outer = Outputer(functioneer)
+    print outer.out()
 
 
 def parse():
@@ -16,25 +31,6 @@ def load(filename):
     with open(filename, 'r') as ff:
         return ff.readlines()
 
-
-def output(fns):
-    lines = []
-    for fn in fns:
-        for xx, ll in enumerate(fn['lines']):
-            app = ""
-            if xx == 0:
-                app = ";FN:" + fn['name']
-            lines.append(ll + app)
-        lines.append('RTN')
-    return lines
-
-
-def main():
-    args = parse()
-    functioneer = Functioneer(load(args.file))
-    functioneer.get_functions()
-    outer = Outputer(functioneer)
-    print outer.out()
 
 if __name__ == "__main__":
     main()
