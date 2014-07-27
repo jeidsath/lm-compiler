3e4f8f1718609682a059d94dad34298b77ab467c
Joel Eidsath <jeidsath@gmail.com>
2014-07-27 14:31:28 -0700

    interpolating language

--- macroer.py
+++ macroer.py
@@ -1,5 +1,8 @@
 import re
 import util
+import subprocess
+import sys
+from StringIO import StringIO
 
 
 class Macroer(object):
@@ -27,6 +30,8 @@ class Macroer(object):
             self.macros.append(macro)
 
     def replaceMacros(self):
+        self.replacePrograms()
+
         self.loadMacros()
 
         for mm in self.macros:
@@ -35,4 +40,37 @@ class Macroer(object):
                 newText.append(ll.replace(mm[0], "\n".join(mm[1:])))
             self.text = newText
 
+
         return self.text
+
+    def replacePrograms(self):
+        idx, block = self.get_block()
+        while idx:
+            output = self.execute_block(block)
+            self.replace_block(idx, len(block) + 2, output)
+            idx, block = self.get_block()
+
+    def get_block(self):
+        start = None
+        end = None
+        for xx, ll in enumerate(self.text):
+            if not start:
+                if util.real_strip(ll) == '<$':
+                    start = xx
+            if not end:
+                if util.real_strip(ll) == '$>':
+                    end = xx
+        if start:
+            return start, self.text[start + 1:end]
+        else:
+            return None, None
+
+    def execute_block(self, block):
+        buffer = StringIO()
+        sys.stdout = buffer
+        exec "\n".join(block)
+        sys.stdout = sys.__stdout__
+        return buffer.getvalue()
+
+    def replace_block(self, idx, size, replacement):
+        self.text = self.text[0:idx] + replacement.split("\n") + self.text[idx + size:]
